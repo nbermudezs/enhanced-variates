50136f2c26ff1ed6f8d67039d34648a0efb9900a
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-30 11:32:44 -0600

    Add BracketGenerator class

---
+++ experiments/BracketGenerator.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#include "BracketGenerator.h"
+
+BracketGenerator::BracketGenerator() {
+    cpt = new ConditionalProbabilityTable();
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    generator = default_random_engine(seed);
+}
+
+
+Bracket* BracketGenerator::get() {
+    vector<int> data = {};
+    for (int matchId = 0; matchId < VECTOR_SIZE; matchId++) {
+        float p = cpt->P(matchId);
+        data.push_back(distribution(generator) < p ? 1 : 0);
+    }
+    return new Bracket(data);
+}
---
+++ experiments/BracketGenerator.h
@@ -0,0 +1,30 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_BRACKETGENERATOR_H
+#define EXPERIMENTS_BRACKETGENERATOR_H
+
+
+#include <random>
+#include "Bracket.h"
+#include "ConditionalProbabilityTable.h"
+
+using namespace std;
+
+
+class BracketGenerator {
+public:
+    BracketGenerator();
+    Bracket* get();
+private:
+    ConditionalProbabilityTable* cpt;
+    default_random_engine generator;
+    uniform_real_distribution<float> distribution;
+    const int VECTOR_SIZE = 63;
+};
+
+
+#endif //EXPERIMENTS_BRACKETGENERATOR_H
