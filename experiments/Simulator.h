3cdc4e63cc327270724634f04102c8dc681945e4
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-30 11:45:36 -0600

    Add empty Simulator class

---
+++ experiments/Simulator.h
@@ -0,0 +1,16 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_SIMULATOR_H
+#define EXPERIMENTS_SIMULATOR_H
+
+
+class Simulator {
+
+};
+
+
+#endif //EXPERIMENTS_SIMULATOR_H
