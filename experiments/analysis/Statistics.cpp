6b29d893e89dfac0fe4779a626d88e46e0652c8b
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-30 12:22:52 -0600

    Add empty Statistics class

---
+++ experiments/analysis/Statistics.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#include "Statistics.h"
---
+++ experiments/analysis/Statistics.h
@@ -0,0 +1,16 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_STATISTICS_H
+#define EXPERIMENTS_STATISTICS_H
+
+
+class Statistics {
+
+};
+
+
+#endif //EXPERIMENTS_STATISTICS_H
