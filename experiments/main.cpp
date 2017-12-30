39e99c5feec06d95e60e59afe51bfdf8b96135d8
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-30 12:07:56 -0600

    Run Simulator from main.cpp

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -2,6 +2,7 @@
 #include "BracketGenerator.h"
 #include "BracketReader.h"
 #include "Scorer.h"
+#include "Simulator.h"
 
 using namespace std;
 
@@ -17,11 +18,18 @@ ostream &operator<<(ostream &os, vector<int> &v)
 int main() {
     BracketGenerator generator;
 
-    Bracket* reference = BracketReader::read("some_path");
+    Bracket* reference = BracketReader::read("reference_bracket_path");
     Bracket* input = generator.get();
     int score = Scorer::eval(reference, input);
     cout << "Reference: " << reference->data;
     cout << "Generated: " << input->data;
     cout << "Score: " << score << endl;
+
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    Simulator simulator;
+    simulator.run("reference_bracket_path");
+    chrono::steady_clock::time_point end= chrono::steady_clock::now();
+    cout << "Took " << chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms";
+
     return 0;
 }
