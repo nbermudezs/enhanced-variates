36b95d571bfdb73997560935a56ce2e01e842475
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-30 11:32:27 -0600

    Add ConditionalProbabilityTable class

---
+++ experiments/ConditionalProbabilityTable.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#include "ConditionalProbabilityTable.h"
+
+// TODO: default to probability 0.5 instead of the hardcoded value in ConditionalProbabilityTable::P
+ConditionalProbabilityTable::ConditionalProbabilityTable() {
+
+}
+
+// TODO: load probability table from a file (result of Ian's data)
+ConditionalProbabilityTable::ConditionalProbabilityTable(string filepath) {
+
+}
+
+float ConditionalProbabilityTable::P(int matchId) {
+    return 0.5;
+}
---
+++ experiments/ConditionalProbabilityTable.h
@@ -0,0 +1,26 @@
+//
+// Created by Nestor Bermudez Sarmiento on 12/30/17.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_CONDITIONALPROBABILITYTABLE_H
+#define EXPERIMENTS_CONDITIONALPROBABILITYTABLE_H
+
+
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class ConditionalProbabilityTable {
+public:
+    ConditionalProbabilityTable();
+    ConditionalProbabilityTable(string);
+    float P(int);
+private:
+    vector<float> probabilities;
+};
+
+
+#endif //EXPERIMENTS_CONDITIONALPROBABILITYTABLE_H
