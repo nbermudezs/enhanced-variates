6f0901a31cefe285ab1c8863ad17a57cfcd64171
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-01-06 16:55:06 -0600

    Add test file for bitset class behavior

---
+++ experiments/test/BitsetTest.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Nestor Bermudez Sarmiento on 1/4/18.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#include "BitsetTest.h"
---
+++ experiments/test/BitsetTest.h
@@ -0,0 +1,18 @@
+//
+// Created by Nestor Bermudez Sarmiento on 1/4/18.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_BITSETTEST_H
+#define EXPERIMENTS_BITSETTEST_H
+
+
+
+class BitsetTest {
+
+};
+
+
+
+#endif //EXPERIMENTS_BITSETTEST_H
