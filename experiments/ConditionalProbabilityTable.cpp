dfebaa984bf12108406251add2eab7d599f9b83d
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-01-06 17:04:26 -0600

    Add seed probabilities (unused for now)

--- experiments/ConditionalProbabilityTable.cpp
+++ experiments/ConditionalProbabilityTable.cpp
@@ -18,4 +18,39 @@ ConditionalProbabilityTable::ConditionalProbabilityTable(string filepath) {
 
 float ConditionalProbabilityTable::P(int matchId) {
     return 0.5;
+    if (matchId >= 32) {
+        return 0.5;
+    }
+    // seed 1
+    if (matchId % 8 == 0) {
+        return 1;
+    }
+    // seed 2
+    if (matchId % 8 == 7) {
+        return 0.939;
+    }
+    // seed 3
+    if (matchId % 8 == 5) {
+        return 0.841;
+    }
+    // seed 4
+    if (matchId % 8 == 3) {
+        return 0.803;
+    }
+    // seed 5
+    if (matchId % 8 == 2) {
+        return 0.644;
+    }
+    // seed 6
+    if (matchId % 8 == 4) {
+        return 0.629;
+    }
+    // seed 7
+    if (matchId % 8 == 6) {
+        return 0.614;
+    }
+    // seed 8
+    if (matchId % 8 == 1) {
+        return 0.508;
+    }
 }
