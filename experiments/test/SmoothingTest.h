36c1f12c7180117da24ec87d985ee95f799b700f
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-01-15 21:51:24 -0600

    Add baseline results for 1e5 w/o pooling

---
+++ experiments/test/SmoothingTest.h
@@ -0,0 +1,16 @@
+//
+// Created by Nestor Bermudez Sarmiento on 1/14/18.
+// nab6@illinois.edu, nestor.bermudezs@gmail.com
+// University of Illinois at Urbana-Champaign
+//
+
+#ifndef EXPERIMENTS_SMOOTHINGTEST_H
+#define EXPERIMENTS_SMOOTHINGTEST_H
+
+
+class SmoothingTest {
+
+};
+
+
+#endif //EXPERIMENTS_SMOOTHINGTEST_H
