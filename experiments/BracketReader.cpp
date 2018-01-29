86787d6da95cbafb8897b2de4b5f914bfa103eeb
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-01-29 16:29:22 -0600

    Allow bracket to be read from JSON file for multiple years

--- experiments/BracketReader.cpp
+++ experiments/BracketReader.cpp
@@ -7,11 +7,30 @@
 #include "BracketReader.h"
 
 
-Bracket* BracketReader::read(string filePath) {
+Bracket* BracketReader::readSingle(string filePath) {
     ifstream inFile(BASE_PATH + filePath);
     string vectorData;
     getline(inFile, vectorData);
     BracketData data(vectorData);
     inFile.close();
     return new Bracket(data);
+}
+
+Bracket *BracketReader::readSingle(string filePath, int year) {
+    ifstream file(BASE_PATH + filePath);
+    CEREAL_RAPIDJSON_NAMESPACE::IStreamWrapper isw(file);
+    CEREAL_RAPIDJSON_NAMESPACE::Document d;
+    d.ParseStream(isw);
+
+    const CEREAL_RAPIDJSON_NAMESPACE::Value& root = d["brackets"];
+    assert(root.IsArray());
+
+    for (unsigned int i = 0; i < root.Size(); i++) {
+        const CEREAL_RAPIDJSON_NAMESPACE::Value& bracket = root[i]["bracket"];
+        int bracketYear = stoi(bracket["year"].GetString());
+        if (bracketYear == year)
+            return new Bracket(BracketData(bracket["fullvector"].GetString()));
+    }
+
+    return nullptr;
 }
--- experiments/BracketReader.h
+++ experiments/BracketReader.h
@@ -8,6 +8,8 @@
 #define EXPERIMENTS_BRACKETREADER_H
 
 
+#include <cereal/external/rapidjson/document.h>
+#include <cereal/external/rapidjson/istreamwrapper.h>
 #include <fstream>
 #include <string>
 #include "Bracket.h"
@@ -16,7 +18,26 @@ using namespace std;
 
 class BracketReader {
 public:
-    static Bracket* read(string);
+
+    /**
+        Returns the bracket from the given file.
+        The file should be a text file only containing the 0/1 vector representation of the bracket
+
+        @param filePath The path of the file to load the bracket from
+        @return The Bracket object
+    */
+    static Bracket* readSingle(string filePath);
+
+    /**
+        Returns the bracket from the given file.
+        The file should be a JSON file that contains a list of brackets. Each bracket sub-document
+        should have a year and a fullvector keys.
+
+        @param filePath The path of the file to load the brackets from
+        @param year The specific year to be selected from the list of brackets
+        @return The Bracket object
+    */
+    static Bracket* readSingle(string filePath, int year);
 };
 
 
