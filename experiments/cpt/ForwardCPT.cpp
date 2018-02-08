af6ba9eb474c498a70793b9f498aec5401f4f463
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-02-07 18:44:43 -0600

    Implement forward CPT class

--- experiments/cpt/ForwardCPT.cpp
+++ experiments/cpt/ForwardCPT.cpp
@@ -6,3 +6,121 @@
  */
 
 #include "ForwardCPT.h"
+
+map<string, ForwardCPT*> ForwardCPT::instances;
+
+ForwardCPT::ForwardCPT(string filePath, bool isMetadataFile, int year) {
+    this->isMetadataFile = isMetadataFile;
+
+    ifstream file(filePath);
+    CEREAL_RAPIDJSON_NAMESPACE::IStreamWrapper isw(file);
+    CEREAL_RAPIDJSON_NAMESPACE::Document d;
+    d.ParseStream(isw);
+
+    if (isMetadataFile) {
+        const CEREAL_RAPIDJSON_NAMESPACE::Value& array = d["sumOfBrackets"];
+        assert(array.IsArray());
+        for (unsigned int i = 0; i < VECTOR_SIZE; i++) {
+            this->probabilities.push_back(1.0 * array[i].GetFloat() / YEARS);
+        }
+    } else {
+        const CEREAL_RAPIDJSON_NAMESPACE::Value& root = d["brackets"];
+        assert(root.IsArray());
+
+        /* stupid initialization because C++ does not have an equivalent to defaultdict */
+        vector<pair<int, int>> pairs = { make_pair(-1, -1), make_pair(0, 0), make_pair(0, 1), make_pair(1, 0), make_pair(1, 1) };
+        for (unsigned int i = 0; i < VECTOR_SIZE; i++) {
+            for (auto p: pairs) {
+                this->conditionalCounts[i][p] = 1;
+                this->parentCounts[i][p] = 1;
+            }
+        }
+
+        for (unsigned int i = 0; i < root.Size(); i++) {
+            const CEREAL_RAPIDJSON_NAMESPACE::Value &bracket = root[i]["bracket"];
+            int bracketYear = stoi(bracket["year"].GetString());
+            if (bracketYear == year)
+                continue;
+            string vector = bracket["fullvector"].GetString();
+            for (unsigned int bit = 0; bit < VECTOR_SIZE; bit++) {
+                int sibling = this->getSiblingBit(bit);
+                pair<int, int> thisPairValues;
+                if (sibling > bit)
+                    thisPairValues = make_pair(vector[bit] - '0', vector[sibling] - '0');
+                else
+                    thisPairValues = make_pair(vector[sibling] - '0', vector[bit] - '0');
+                if (bit < 60 && bit % 15 < 8)
+                    if (vector[bit] - '0' == 1)
+                        this->parentCounts[bit][make_pair(-1, -1)] += 1;
+                else
+                    this->parentCounts[bit][thisPairValues] += 1;
+
+                pair<int, int> parents = this->getParentBits(bit);
+                pair<int, int> parentValues = make_pair(vector[parents.first] - '0', vector[parents.second] - '0');
+
+                if (vector[bit] - '0' == 1)
+                    this->conditionalCounts[bit][parentValues] += 1;
+            }
+        }
+    }
+}
+
+double ForwardCPT::P(int bitId) {
+    if (this->overrides[bitId])
+        return this->overrides[bitId];
+    return this->probabilities[bitId];
+}
+
+double ForwardCPT::P(int bitId, BracketData data) {
+    if (this->overrides[bitId])
+        return this->overrides[bitId];
+
+    if (this->isMetadataFile)
+        return this->P(bitId);
+
+
+    pair<int, int> parents = this->getParentBits(bitId);
+    if (parents.first < 0)
+        return 1. * this->parentCounts[bitId][parents] / YEARS;
+    pair<int, int> values = make_pair(data[VECTOR_SIZE - parents.first - 1], data[VECTOR_SIZE - parents.second - 1]);
+    int count = this->conditionalCounts[bitId][values];
+    int parentsCount = this->parentCounts[parents.first][values];
+    return 1. * count / parentsCount;
+}
+
+/* PRIVATE */
+pair<int, int> ForwardCPT::getParentBits(int bitId) {
+    if (bitId == 62) return make_pair(60, 61);
+    if (bitId == 61) return make_pair(44, 59);
+    if (bitId == 60) return make_pair(14, 29);
+    if (bitId % 15 < 8) return make_pair(-1, -1);
+
+    int mod = bitId % 15;
+    if (mod == 14) return make_pair(bitId - 2, bitId - 1);
+
+    int factor = bitId / 15;
+    int base;
+    if (mod < 12) base = (mod % 8) * 2 + 15 * factor;
+    else if (mod < 14) base = 8 + (mod % 12) * 2 + 14 * factor;
+    return make_pair(base, base + 1);
+}
+
+int ForwardCPT::getSiblingBit(int bitId) {
+    if (bitId == 62) return -1;
+    if (bitId == 60) return 61;
+    if (bitId == 14) return 29;
+    if (bitId == 29) return 14;
+    if (bitId == 44) return 60;
+    if (bitId == 59) return 44;
+    int delta = (bitId % 15) % 2 == 0 ? 1 : -1;
+    return bitId + delta;
+}
+
+ForwardCPT &ForwardCPT::getInstance(string filePath, bool isMetadataFile, int year, map<int, double> overrides,
+                                    string instanceKey) {
+    if (!instances[instanceKey]) {
+        ForwardCPT *instance = new ForwardCPT(filePath, isMetadataFile, year, overrides);
+        instances[instanceKey] = instance;
+    }
+    return *instances[instanceKey];
+}
--- experiments/cpt/ForwardCPT.h
+++ experiments/cpt/ForwardCPT.h
@@ -8,9 +8,133 @@
 #ifndef EXPERIMENTS_FORWARDCPT_H
 #define EXPERIMENTS_FORWARDCPT_H
 
+#include "BaseCPT.h"
+using namespace std;
 
-class ForwardCPT {
 
+class ForwardCPT: public BaseCPT {
+public:
+    /**
+     * Returns the unconditional probability of the @param{bitId} being 1
+     * @param bitId index of the bit. @see Constants::VECTOR_SIZE
+     * @return the probability of the @param{bitId} = 1.
+     */
+    double P(int bitId);
+
+    /**
+     * Returns the conditional probability of the @param{bitId} being 1
+     * @param bitId index of the bit. @see Constants::VECTOR_SIZE
+     * @param bracket bracket generated so far, the conditional information
+     * to find P(bit = 1) will be taken from this.
+     * @return the conditional probability of the given bit being 1.
+     */
+    double P(int bitId, BracketData data);
+
+    /**
+     * Returns an instance of this class based on the provided attributes.
+     * This implements the singleton pattern design so the same instance is returned
+     * given the @param{instanceKey}
+     * @param filePath file containing the brackets or metadata
+     * @param isMetadataFile whether the provided @param{filePath} corresponds to
+     * a metadata file or not. If false, the file should be a JSON file.
+     * @param year year to be omitted. Only in effect if @param{isMetadataFile} is false.
+     * @param overrides a map where the keys are bit ids and the values are the expected probability
+     * of such bit being 1. These probabilities override whatever probability computed from
+     * the provided file.
+     * @param instanceKey key associated with this set of parameters. Given a key the
+     * same instance will be returned every time this method is called.
+     * @return an instance of this class.
+     */
+    static ForwardCPT& getInstance(string filePath, bool isMetadataFile, int year,
+                                   map<int, double> overrides, string instanceKey);
+
+    /**
+     * Constructs an empty instance of this class.
+     */
+    ForwardCPT() = default;
+
+    /**
+     * Creates an instance of this class
+     * @param filePath path of the file containing the brackets' info
+     * @param isMetadataFile boolean flag that indicates whether the provided file
+     * already has the summarized counts and there is no need to process the
+     * individual brackets. If this flag is false the file is expected to contain all
+     * the brackets that should be analyzed.
+     * @param year indicates which year, if any, must be omitted from the brackets
+     * contained in the file. This argument only has effect when isMetadataFile is false.
+     */
+    ForwardCPT(string filePath, bool isMetadataFile, int year);
+
+    /**
+     * Creates an instance of this class
+     * @param filePath path of the file containing the brackets' info
+     * @param isMetadataFile boolean flag that indicates whether the provided file
+     * already has the summarized counts and there is no need to process the
+     * individual brackets. If this flag is false the file is expected to contain all
+     * the brackets that should be analyzed.
+     * @param year indicates which year, if any, must be omitted from the brackets
+     * contained in the file. This argument only has effect when isMetadataFile is false.
+     * @param overrides a map where the keys are bit ids and the values are the expected probability
+     * of such bit being 1. These probabilities override whatever probability computed from
+     * the provided file.
+     */
+    ForwardCPT(string filePath, bool isMetadataFile, int year, map<int, double> overrides):
+            ForwardCPT(filePath, isMetadataFile, year) {
+        this->overrides = overrides;
+    };
+
+    /**
+     * Specifies at which bit the generation process should start
+     * @return the start bit (0)
+     */
+    int startBit() { return 0; }
+
+    /**
+     * Specifies at which bit the generation process should stop
+     * @return the end bit (Constants::VECTOR_SIZE - 1)
+     */
+    int endBit() { return VECTOR_SIZE - 1; }
+
+    /**
+     * Determines whether the traversing of the vector happens from left to right or vice-versa.
+     * @return delta (1)
+     */
+    int bitAdvance() { return 1; }
+
+private:
+    /**
+     * Finds the index of the two parents (previous round) associated to the given @param{bitId}
+     * @param bitId index of the bit whose parents we are looking for.
+     * @return a pair of two indices corresponding to the parents' positions in the vector. The first index is always
+     * less than the second one.
+     */
+    pair<int, int> getParentBits(int bitId);
+
+    /**
+     * Finds the bit index that corresponds to the other match whose winner will play against the winner of
+     * the provided bit index
+     * @param bitId index of the bit used as reference
+     * @return the index of the other bit
+     */
+    int getSiblingBit(int bitId);
+
+    /**
+     * conditionalCounts keep track of how many times a particular bit was 1 given that both parents (from the
+     * previous round) where a particular value. Since there are only two parents the key for this map can have only
+     * four different combinations.
+     * The size should be of size Constants::VECTOR_SIZE and the i-th element should correspond to the
+     * conditional counts for the i-th bit. The bit index is the first key of this map.
+     */
+    map<int, map<pair<int, int>, int>> conditionalCounts;
+
+    /**
+     * parentCounts holds how many times an specific configuration of two parents appeared among
+     * the years considered.
+     * The i-th element of this vector corresponds to the i-th bit, the counts correspond to two parents but given
+     * that the positions are fixed we only need one. We will use the lower index of the two.
+     */
+    map<int, map<pair<int, int>, int>> parentCounts;
+    static map<string, ForwardCPT*> instances;
 };
 
 
