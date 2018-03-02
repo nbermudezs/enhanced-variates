36635426753788bf40ae3ccfb7f90d5fb7d25bc5
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-03-02 13:02:01 -0600

    Add support for intra-replication CRN/ARN groups

--- experiments/BracketGenerator.h
+++ experiments/BracketGenerator.h
@@ -18,6 +18,7 @@
 #include "Bracket.h"
 #include "cpt/BackwardCPT.h"
 #include "cpt/ForwardCPT.h"
+#include "cpt/MixedCPT.h"
 #include "GeneratorConfig.h"
 
 using namespace std;
@@ -29,6 +30,7 @@ public:
     BracketGenerator(GenerationDirection generationDirection, string format, int year);
     Bracket* get();
     Bracket* get(bool, GeneratorConfig, vector<VariateMethod>);
+    Bracket* get(GeneratorConfig config);
     BaseCPT* cpt;
     map<int, int> bitOnCounts;
 private:
