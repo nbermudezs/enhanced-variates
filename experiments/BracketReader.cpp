4e6b99569f0a8eb71c8f24307088b336711e9671
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-03-02 10:05:53 -0600

    Add ability to read ESPN score ranks from file

--- experiments/BracketReader.cpp
+++ experiments/BracketReader.cpp
@@ -56,3 +56,51 @@ map<int, Bracket *> BracketReader::readAll(string filePath) {
 
     return result;
 }
+
+map<int, int> BracketReader::readESPNScores(string filePath, int year) {
+    map<int, int> result;
+
+    ifstream file(BASE_PATH + filePath);
+    CEREAL_RAPIDJSON_NAMESPACE::IStreamWrapper isw(file);
+    CEREAL_RAPIDJSON_NAMESPACE::Document d;
+    d.ParseStream(isw);
+
+    const CEREAL_RAPIDJSON_NAMESPACE::Value& root = d["scores"];
+    assert(root.IsArray());
+
+    for (unsigned int i = 0; i < root.Size(); i++) {
+        auto& yearData = root[i];
+        int itemYear = yearData["year"].GetInt();
+        if (itemYear != year)
+            continue;
+        auto& ranks = yearData["rank"];
+        for (auto itr = ranks.MemberBegin(); itr != ranks.MemberEnd(); ++itr) {
+            result[stoi(itr->name.GetString())] = itr->value.GetInt();
+        }
+    }
+
+    return result;
+}
+
+map<int, map<int, int>> BracketReader::readESPNScores(string filePath) {
+    map<int, map<int, int>> result;
+
+    ifstream file(BASE_PATH + filePath);
+    CEREAL_RAPIDJSON_NAMESPACE::IStreamWrapper isw(file);
+    CEREAL_RAPIDJSON_NAMESPACE::Document d;
+    d.ParseStream(isw);
+
+    const CEREAL_RAPIDJSON_NAMESPACE::Value& root = d["scores"];
+    assert(root.IsArray());
+
+    for (unsigned int i = 0; i < root.Size(); i++) {
+        auto& yearData = root[i];
+        int itemYear = yearData["year"].GetInt();
+        auto& ranks = yearData["rank"];
+        for (auto itr = ranks.MemberBegin(); itr != ranks.MemberEnd(); ++itr) {
+            result[itemYear][stoi(itr->name.GetString())] = itr->value.GetInt();
+        }
+    }
+
+    return result;
+}
