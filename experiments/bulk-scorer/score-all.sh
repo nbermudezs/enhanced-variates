97329adecdab8cc1e4e3df5b066df908f6f58c49
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-04-03 17:27:12 -0500

    Keep threshold set to 1300, add bash line

--- experiments/bulk-scorer/score-all.sh
+++ experiments/bulk-scorer/score-all.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 # MAKE SURE YOU HAVE BUILT THE BULK-SCORER TARGET BEFORE RUNNING THIS SCRIPT
 # DATA IS EXPECTED TO BE INSIDE A "data" FOLDER AT THE SAME LEVEL AS THIS SCRIPT
 
+rm bulk-scorer
 ln ../cmake-build-debug/bulk-scorer bulk-scorer
 
 ./bulk-scorer data/10kERNModel-0101100.csv data/10kERNModel-0101100_scored.csv ERN_0101100 10000
@@ -12,17 +14,17 @@ ln ../cmake-build-debug/bulk-scorer bulk-scorer
 ./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_scored.csv ERN_1100111 100000
 ./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_scored.csv ERN_1100111 100000
 
-./bulk-scorer data/100kERNModel-0101100.csv data/100kERNModel-0101100_1000_scored.csv ERN_0101100 100000 1000
-./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_1000_scored.csv ERN_1100111 100000 1000
-./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_1000_scored.csv ERN_1100111 100000 1000
+#./bulk-scorer data/100kERNModel-0101100.csv data/100kERNModel-0101100_1000_scored.csv ERN_0101100 100000 1000
+#./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_1000_scored.csv ERN_1100111 100000 1000
+#./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_1000_scored.csv ERN_1100111 100000 1000
 
 ./bulk-scorer data/100kERNModel-0101100.csv data/100kERNModel-0101100_1300_scored.csv ERN_0101100 100000 1300
 ./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_1300_scored.csv ERN_1100111 100000 1300
 ./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_1300_scored.csv ERN_1100111 100000 1300
 
-./bulk-scorer data/100kERNModel-0101100.csv data/100kERNModel-0101100_1400_scored.csv ERN_0101100 100000 1400
-./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_1400_scored.csv ERN_1100111 100000 1400
-./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_1400_scored.csv ERN_1100111 100000 1400
+#./bulk-scorer data/100kERNModel-0101100.csv data/100kERNModel-0101100_1400_scored.csv ERN_0101100 100000 1400
+#./bulk-scorer data/100kERNModel-1100111.csv data/100kERNModel-1100111_1400_scored.csv ERN_1100111 100000 1400
+#./bulk-scorer data/100kBackwardPModel.csv data/100kBackwardPModel_1400_scored.csv ERN_1100111 100000 1400
 
 # ===============================================================================================================================================
 
@@ -30,17 +32,17 @@ ln ../cmake-build-debug/bulk-scorer bulk-scorer
 ./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_scored.csv ERN_1100111 1000000
 ./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_scored.csv ERN_1100111 1000000
 
-./bulk-scorer data/1milERNModel-0101100.csv data/1milERNModel-0101100_1000_scored.csv ERN_0101100 1000000 1000
-./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_1000_scored.csv ERN_1100111 1000000 1000
-./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_1000_scored.csv ERN_1100111 1000000 1000
+#./bulk-scorer data/1milERNModel-0101100.csv data/1milERNModel-0101100_1000_scored.csv ERN_0101100 1000000 1000
+#./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_1000_scored.csv ERN_1100111 1000000 1000
+#./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_1000_scored.csv ERN_1100111 1000000 1000
 
 ./bulk-scorer data/1milERNModel-0101100.csv data/1milERNModel-0101100_1300_scored.csv ERN_0101100 1000000 1300
 ./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_1300_scored.csv ERN_1100111 1000000 1300
 ./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_1300_scored.csv ERN_1100111 1000000 1300
 
-./bulk-scorer data/1milERNModel-0101100.csv data/1milERNModel-0101100_1400_scored.csv ERN_0101100 1000000 1400
-./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_1400_scored.csv ERN_1100111 1000000 1400
-./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_1400_scored.csv ERN_1100111 1000000 1400
+#./bulk-scorer data/1milERNModel-0101100.csv data/1milERNModel-0101100_1400_scored.csv ERN_0101100 1000000 1400
+#./bulk-scorer data/1milERNModel-1100111.csv data/1milERNModel-1100111_1400_scored.csv ERN_1100111 1000000 1400
+#./bulk-scorer data/1milBackwardPModel.csv data/1milBackwardPModel_1400_scored.csv ERN_1100111 1000000 1400
 
 # ===============================================================================================================================================
 
@@ -52,10 +54,10 @@ ln ../cmake-build-debug/bulk-scorer bulk-scorer
 ./bulk-scorer data/10milERNModel-1100111.csv data/10milERNModel-1100111_1300_scored.csv ERN_1100111 10000000 1300
 ./bulk-scorer data/10milBackwardPModel.csv data/10milBackwardPModel_1300_scored.csv ERN_1100111 10000000 1300
 
-./bulk-scorer data/10milERNModel-0101100.csv data/10milERNModel-0101100_1400_scored.csv ERN_0101100 10000000 1400
-./bulk-scorer data/10milERNModel-1100111.csv data/10milERNModel-1100111_1400_scored.csv ERN_1100111 10000000 1400
-./bulk-scorer data/10milBackwardPModel.csv data/10milBackwardPModel_1400_scored.csv ERN_1100111 10000000 1400
+#./bulk-scorer data/10milERNModel-0101100.csv data/10milERNModel-0101100_1400_scored.csv ERN_0101100 10000000 1400
+#./bulk-scorer data/10milERNModel-1100111.csv data/10milERNModel-1100111_1400_scored.csv ERN_1100111 10000000 1400
+#./bulk-scorer data/10milBackwardPModel.csv data/10milBackwardPModel_1400_scored.csv ERN_1100111 10000000 1400
 
-./bulk-scorer data/10milERNModel-0101100.csv data/10milERNModel-0101100_1000_scored.csv ERN_0101100 10000000 1000
-./bulk-scorer data/10milERNModel-1100111.csv data/10milERNModel-1100111_1000_scored.csv ERN_1100111 10000000 1000
-./bulk-scorer data/10milBackwardPModel.csv data/10milBackwardPModel_1000_scored.csv ERN_1100111 10000000 1000
+#./bulk-scorer data/10milERNModel-0101100.csv data/10milERNModel-0101100_1000_scored.csv ERN_0101100 10000000 1000
+#./bulk-scorer data/10milERNModel-1100111.csv data/10milERNModel-1100111_1000_scored.csv ERN_1100111 10000000 1000
+#./bulk-scorer data/10milBackwardPModel.csv data/10milBackwardPModel_1000_scored.csv ERN_1100111 10000000 1000
